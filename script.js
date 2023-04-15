098269d534b574ec0538ffad69d2cfde7167e163
Daniel J <68441865+djewel23@users.noreply.github.com>
2023-04-14 19:57:58 -0600

    Begin to create dice rolling functionality

--- script.js
+++ script.js
@@ -3,9 +3,35 @@
 // Selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+let currentScore = 0;
 
 // Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function(){
+  // 1. Generate random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
+  // 2. Display dice face
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3. Check for a rolled 1
+  if(dice !== 1){
+    // Add current dice roll to current score
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+    // Switch to the other player
+  }
+});
