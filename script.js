7206e85bbeda0c4abb65526534150ad98e16555f
Daniel J <68441865+djewel23@users.noreply.github.com>
2023-04-17 13:45:44 -0600

    Created init function. Finished up.

--- script.js
+++ script.js
@@ -11,15 +11,27 @@ const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
 
-// Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
+let scores, currentScore, activePlayer, playing;
+
+// Initialization
+const init = function(){
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  player0El.classList.remove(`player--winner`);
+  player1El.classList.remove(`player--winner`);
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+  diceEl.classList.add("hidden");
+};
+// Run initialization on load
+init();
 
 // Change the player
 const switchPlayer = function () {
@@ -61,7 +73,7 @@ btnHold.addEventListener("click", function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     // 2. Check if player's score is >= 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       // Finish the game
       playing = false;
       diceEl.classList.add('hidden');
@@ -73,3 +85,6 @@ btnHold.addEventListener("click", function () {
     }
   }
 });
+
+// Reset the game
+btnNew.addEventListener('click', init);
