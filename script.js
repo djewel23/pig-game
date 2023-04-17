ff061588df8665703c35c6c5844a8b2019fcc0e7
Daniel J <68441865+djewel23@users.noreply.github.com>
2023-04-17 12:59:14 -0600

    Added in steps for else statements on dice roll

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -9,7 +11,9 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Starting conditions
 score0El.textContent = 0;
@@ -30,8 +34,13 @@ btnRoll.addEventListener('click', function(){
   if(dice !== 1){
     // Add current dice roll to current score
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
   } else {
     // Switch to the other player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
